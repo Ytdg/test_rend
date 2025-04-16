ac5d5c9bf395702729d67320f651896e3f165fea
Nikita Bochkov <nikiljgryt@gmail.com>
2025-04-16 04:25:23 +0300

    test

---
+++ src/main/java/com/hackaton/aliceskill/AliceSkillApplication.java
@@ -0,0 +1,17 @@
+package com.hackaton.aliceskill;
+
+import lombok.Data;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliceSkillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AliceSkillApplication.class, args);
+    }
+
+}
+
+//https://public_ip/
+//aws
---
+++ src/main/java/com/hackaton/aliceskill/TestController.java
@@ -0,0 +1,7 @@
+package com.hackaton.aliceskill;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
